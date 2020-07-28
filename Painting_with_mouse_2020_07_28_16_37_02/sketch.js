9b22f03c5029632ef931b3f47351518a53c1a10c
kohbeining1 <e0324853@u.nus.edu>
2020-07-29 00:39:32 +0800

    bunch of updates

---
+++ Painting_with_mouse_2020_07_28_16_37_02/sketch.js
@@ -0,0 +1,28 @@
+var hue;
+var rainbow = true;
+var rate = .2;
+
+
+function setup() {
+  createCanvas(1918,4000);
+  background(199, 178, 153);
+  hue = 55;
+}  
+
+function draw() {
+  // background(220);
+}
+
+function draw() {
+    if (rainbow) {
+    if (hue >90) {
+        hue = 55;
+    } else {
+        hue += rate;
+    }
+	}
+  colorMode(HSL, 360);
+  noStroke();
+  fill(hue, 170, 250);
+  ellipse(mouseX, mouseY, 400, 400);
+}
